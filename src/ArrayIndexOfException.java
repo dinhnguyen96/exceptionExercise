5650c61803f611c30ca9f1b22c7a72302b894b1c
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-02-02 16:01:33 +0700

    add ArrayIndexOfException exercise

---
+++ src/ArrayIndexOfException.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayIndexOfException {
+
+
+
+    public static void createRandom(int[] randomArray)
+    {
+        Random random = new Random();
+        for (int i = 0; i < randomArray.length; i++)
+        {
+             randomArray[i] = random.nextInt(0, 100);
+        }
+    }
+
+    public static void arrayOutput(int[] randomArray)
+    {
+        for (int i = 0; i < randomArray.length; i++)
+        {
+            System.out.println(randomArray[i] + " ");
+        }
+    }
+
+    public static void elementSearchByIndex(int[] randomArray, int index)
+    {
+        if (index < 0 || index >= 100 )
+        {
+            System.out.println("Chỉ số phần tử không hợp lệ");
+            return;
+        }
+        for (int i = 0; i < randomArray.length; i++)
+        {
+            if (i == index)
+            {
+                System.out.printf("Phần tử tại vị trí %d là : %d", i, randomArray[i]);
+                break;
+            }
+        }
+    }
+    public static void indexInput(int[] randomArray)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập vị trí phần tử cần tìm : ");
+        int index = input.nextInt();
+        elementSearchByIndex(randomArray,index);
+    }
+
+
+    public static void main(String[] args)
+    {
+        int[] randomArray = new int[100];
+        createRandom(randomArray);
+        arrayOutput(randomArray);
+        indexInput(randomArray);
+    }
+}
